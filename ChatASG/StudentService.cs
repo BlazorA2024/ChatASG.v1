f5653c0360a1fcc310484a83e090f6663c332dfb
eited <735408478mm@gmail.com>
2025-06-25 21:51:07 +0300

    hbjg

--- ChatASG/StudentService.cs
+++ ChatASG/StudentService.cs
@@ -1,49 +1,98 @@
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Http;
+using System.Net.Http.Headers;
+using System.Security.Claims;
 
 namespace NWHttps
 {
-
     public interface IWebApiClientFactory
     {
-        Task<HttpClient> GetClientAsync();
+      Task<HttpClient> GetClientAsync();
 
     }
-    //https://localhost:6002/swagger/v1/swagger.json
-    //
-
-
-
-
-    public class WebApiClientFactory : IWebApiClientFactory
+ 
+public class WebApiClientFactory : IWebApiClientFactory
     {
         private readonly IHttpClientFactory _httpClientFactory;
-
-
-
+        private readonly IHttpContextAccessor _httpContextAccessor;
 
         public WebApiClientFactory(
-            IHttpClientFactory httpClientFactory
-
-            )
+            IHttpClientFactory httpClientFactory,
+            IHttpContextAccessor httpContextAccessor)
         {
             _httpClientFactory = httpClientFactory;
-
-            // _token = token;
-
+            _httpContextAccessor = httpContextAccessor;
         }
-
-
 
         public async Task<HttpClient> GetClientAsync()
         {
             var httpClient = _httpClientFactory.CreateClient("LocalApi");
 
-            //httpClient.BaseAddress = new Uri(_appSettings.BaseUrls.Api);
-            //  string bearerToken = _token.AccessToken;
-            //  if (!string.IsNullOrEmpty(bearerToken)) httpClient.SetBearerToken(bearerToken);
+            //var token = await _httpContextAccessor.HttpContext?.GetTokenAsync("access_token");
+
+            //if (!string.IsNullOrWhiteSpace(token))
+            //{
+            //    Console.WriteLine($"? [WebApiClientFactory] Bearer Token = {token.Substring(0, 20)}...");
+
+            //    httpClient.DefaultRequestHeaders.Authorization =
+            //        new AuthenticationHeaderValue("Bearer", token);
+            //}
+            //else
+            //{
+            //    Console.WriteLine("? [WebApiClientFactory] Token is NULL or empty!");
+            //}
 
             return httpClient;
-
         }
-
     }
+
 }
+
+//namespace NWHttps
+//{
+
+//    public interface IWebApiClientFactory
+//    {
+//        Task<HttpClient> GetClientAsync();
+
+//    }
+//    //https://localhost:6002/swagger/v1/swagger.json
+//    //
+
+
+
+
+//    public class WebApiClientFactory : IWebApiClientFactory
+//    {
+//        private readonly IHttpClientFactory _httpClientFactory;
+
+
+
+
+//        public WebApiClientFactory(
+//            IHttpClientFactory httpClientFactory
+
+//            )
+//        {
+//            _httpClientFactory = httpClientFactory;
+
+//            // _token = token;
+
+//        }
+
+
+
+//        public async Task<HttpClient> GetClientAsync()
+//        {
+//            var httpClient = _httpClientFactory.CreateClient("LocalApi");
+
+//            //httpClient.BaseAddress = new Uri(_appSettings.BaseUrls.Api);
+//            //  string bearerToken = _token.AccessToken;
+//            //  if (!string.IsNullOrEmpty(bearerToken)) httpClient.SetBearerToken(bearerToken);
+
+//            return httpClient;
+
+//        }
+
+//    }
+//}
